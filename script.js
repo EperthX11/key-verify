7f559bd97552f4e4212df60134bc5db2fb455201
EperthX11 <136739018+EperthX11@users.noreply.github.com>
2023-08-08 19:35:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Your existing JavaScript code
-
-let generateItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
-let generatedItems = [];
-
 // Your key verification code
 function verifyKey() {
     const enteredKey = document.getElementById("keyInput").value;
@@ -16,12 +11,8 @@ function verifyKey() {
     }
 }
 
-function generateText() {
-    // Redirect to the provided URL
-    window.location.href = "https://mega.nz/file/LI0CXD5J#rX2kWbQB-oGxkaLOHQgDLvhYkati4oRWzRJqp-J_hoo";
-}
-
-function showHistory() {
-    const historyDiv = document.getElementById("history");
-    historyDiv.innerHTML = generatedItems.map(item => `<p>${item}</p>`).join("");
+// Generate the link and display it
+function generateLink() {
+    const generatedLink = "https://mega.nz/file/LI0CXD5J#rX2kWbQB-oGxkaLOHQgDLvhYkati4oRWzRJqp-J_hoo";
+    document.getElementById("generatedLink").innerHTML = `<a href="${generatedLink}" target="_blank">Download Link</a>`;
 }
