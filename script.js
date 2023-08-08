c59416bc1638f9f74af8aa98e7e50bd1a0f48285
EperthX11 <136739018+EperthX11@users.noreply.github.com>
2023-08-08 19:04:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-// Your existing JavaScript code
-
-let generateItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
-let generatedItems = [];
-
-function generateText() {
-    const historyDiv = document.getElementById("history");
-
-    if (generateItems.length > 0) {
-        let newItem = generateItems.shift(); // Remove the first item from the array
-        generatedItems.push(newItem);
-
-        if (generateItems.length === 0) {
-            historyDiv.innerHTML = "<p>Out of stock</p>";
-        } else {
-            historyDiv.innerHTML = `<p>${newItem}</p>`;
-        }
-    } else {
-        historyDiv.innerHTML = "<p>Out of stock</p>";
-    }
-}
-
 function verifyKey() {
     const enteredKey = document.getElementById("keyInput").value;
     const correctKey = "500168-492367-110231";
 
     if (enteredKey === correctKey) {
-        // Modify the URL to match your actual generate.html location
         window.location.href = "generate.html";
     } else {
         document.getElementById("errorMessage").textContent = "Incorrect key. Please try again.";
     }
 }
+
+function generateText() {
+    const generatedText = "Generated text goes here.";
+    const historyDiv = document.getElementById("history");
+    historyDiv.innerHTML += `<p>${generatedText}</p>`;
+}
+
+function showHistory() {
+    const historyDiv = document.getElementById("history");
+    historyDiv.style.display = "block";
+}
+
+// Your other existing JavaScript code goes here
+// For example:
+// - Event listeners
+// - Other functions
+// - Additional functionality
