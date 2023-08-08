cf82df7eb9a572e8507056c7bd748dbd1be48eb0
EperthX11 <136739018+EperthX11@users.noreply.github.com>
2023-08-08 19:24:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,7 @@ function verifyKey() {
 }
 
 function generateText() {
-    const historyDiv = document.getElementById("history");
-
-    if (generateItems.length > 0) {
-        let newItem = generateItems.shift(); // Remove the first item from the array
-        generatedItems.push(newItem);
-
-        if (generateItems.length === 0) {
-            historyDiv.innerHTML = "<p>Out of stock</p>";
-        } else {
-            historyDiv.innerHTML = `<p>${newItem}</p>`;
-        }
-
-        // Redirect to the provided URL
-        window.location.href = "https://mega.nz/file/LI0CXD5J#rX2kWbQB-oGxkaLOHQgDLvhYkati4oRWzRJqp-J_hoo";
-    } else {
-        historyDiv.innerHTML = "<p>Out of stock</p>";
-    }
+    // Redirect to the provided URL
+    window.location.href = "https://mega.nz/file/LI0CXD5J#rX2kWbQB-oGxkaLOHQgDLvhYkati4oRWzRJqp-J_hoo";
 }
-
 
