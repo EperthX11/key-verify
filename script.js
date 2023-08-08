8ff29e21008ccde0c68e7e71f522e68b8c3ad763
EperthX11 <136739018+EperthX11@users.noreply.github.com>
2023-08-08 19:01:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function verifyKey() {
     const enteredKey = document.getElementById("keyInput").value;
     const correctKey = "500168-492367-110231";
@@ -19,3 +20,25 @@ function showHistory() {
     const historyDiv = document.getElementById("history");
     historyDiv.style.display = "block";
 }
+
+
+let generateItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+let generatedItems = [];
+
+function generateText() {
+    const historyDiv = document.getElementById("history");
+
+    if (generateItems.length > 0) {
+        let newItem = generateItems.shift(); // Remove the first item from the array
+        generatedItems.push(newItem);
+
+        if (generateItems.length === 0) {
+            historyDiv.innerHTML = "<p>Out of stock</p>";
+        } else {
+            historyDiv.innerHTML = `<p>${newItem}</p>`;
+        }
+    } else {
+        historyDiv.innerHTML = "<p>Out of stock</p>";
+    }
+}
+
