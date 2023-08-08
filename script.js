87e898e9f0e03126f4cc04f7474685541f924178
EperthX11 <136739018+EperthX11@users.noreply.github.com>
2023-08-08 19:25:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// Your existing JavaScript code
+
+let generateItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+let generatedItems = [];
+
+// Your key verification code
 function verifyKey() {
     const enteredKey = document.getElementById("keyInput").value;
     const correctKey = "500168-492367-110231";
 
     if (enteredKey === correctKey) {
+        // Modify the URL to match your actual generate.html location
         window.location.href = "generate.html";
     } else {
         document.getElementById("errorMessage").textContent = "Incorrect key. Please try again.";
@@ -13,4 +20,3 @@ function generateText() {
     // Redirect to the provided URL
     window.location.href = "https://mega.nz/file/LI0CXD5J#rX2kWbQB-oGxkaLOHQgDLvhYkati4oRWzRJqp-J_hoo";
 }
-
